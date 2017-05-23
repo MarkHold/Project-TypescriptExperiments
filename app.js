6963eee40316495cb44f37c72eeea456255d2d69
markuslyconhold <ma223ik@student.lnu.se>
2017-05-23 23:09:22 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by markuslyconhold on 23/05/17.
+ */
+var app = function () {
+    console.log('brbrbr');
+    console.log("brbrb");
+};
+app();
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by markuslyconhold on 23/05/17.
+ */
+
+var app = function(){
+
+    console.log('brbrbr');
+    console.log("brbrb");
+}
+app();
