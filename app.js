4c66131700c7f9b7053952d454127a68725aadd8
markuslyconhold <ma223ik@student.lnu.se>
2017-06-10 18:28:29 +0200

    Some of the basic components of TS

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 /**
  * Created by markuslyconhold on 23/05/17.
  */
-var app = function () {
-    console.log('brbrbr');
-    console.log("brbrb");
+/**
+ * Classes are the same from java, object oriented programming.
+ * Using public on arguments to the constructor is a shorthand that
+ * allows us to automaticlly create properties with that name.
+ */
+var submitButton = document.getElementById("SubmitButton");
+submitButton.onclick = function () {
+    var theLink = document.getElementsByName("LinkTextArea")[0];
+    console.log(theLink);
 };
-app();
+var Student = (function () {
+    function Student(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + lastName;
+    }
+    return Student;
+}());
+/**
+ * Here we have a function that takes in a variable name
+ * person and connects it to the object Person
+ * @param person
+ */
+function greeter(person) {
+    return "Hi, " + person.firstName + " " + person.lastName;
+}
+var classUser = new Student("Murre", "muk");
+var majorDiv = document.getElementById("majorDiv");
+majorDiv.innerHTML = greeter(classUser);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -2,9 +2,47 @@
  * Created by markuslyconhold on 23/05/17.
  */
 
-var app = function(){
 
-    console.log('brbrbr');
-    console.log("brbrb");
+
+/**
+ * Classes are the same from java, object oriented programming.
+ * Using public on arguments to the constructor is a shorthand that
+ * allows us to automaticlly create properties with that name.
+ */
+    var submitButton = document.getElementById("SubmitButton");
+
+submitButton.onclick = function (){
+
+    var theLink = <HTMLScriptElement>document.getElementsByName("LinkTextArea")[0];
+    console.log(theLink);
+};
+class Student{
+    fullName; string;
+    constructor(public firstName, public lastName){
+        this.fullName = firstName + " " + lastName;
+    }
 }
-app();
+
+/**interfaces are used to address
+ * description and fields for objects.
+ * In Typescript, two types are ompatible if their
+ * internal structure is compatible.
+ */
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+/**
+ * Here we have a function that takes in a variable name
+ * person and connects it to the object Person
+ * @param person
+ */
+function greeter(person: Person) {
+    return "Hi, "+person.firstName + " "+ person.lastName;
+}
+
+var classUser = new Student("Murre","muk");
+
+var majorDiv = document.getElementById("majorDiv");
+majorDiv.innerHTML = greeter(classUser);
